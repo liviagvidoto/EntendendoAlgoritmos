bbd0a6d89034eb8008021c847eba3a7eb6f1ccd7
liviagvidoto <liwvidoto@gmail.com>
2026-06-05 18:08:14 -0300

    Programação dinamica sequencia de fibonacci

--- cap9/programacao_dinamica.py
+++ cap9/programacao_dinamica.py
@@ -1,3 +1,12 @@
 # Programação Dinâmica
 # O nome "programação dinâmica" foi cunhado por Richard Bellman na década de 1950. Ele escolheu o termo "dinâmica" para transmitir a ideia de que o processo de resolução de problemas é fluida e adaptativa, e "programação" para se referir à formulação matemática dos problemas. A programação dinâmica é uma técnica poderosa para resolver problemas de otimização e contagem, onde a solução pode ser construída a partir de soluções menores.
  
+def fibonacci(n, memo={}):
+    if n in memo:
+        return memo[n]
+    if n <= 1:
+        return n
+    memo[n] = fibonacci(n-1, memo) + fibonacci(n-2, memo)
+    return memo[n]
+
+print(fibonacci(10))  # Saída: 55
